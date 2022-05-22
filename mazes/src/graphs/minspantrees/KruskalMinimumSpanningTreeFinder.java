cab59d91bd836d7ed3338d49537065dc2ce7d982
Jeremy Cao <you@example.com>
2022-05-21 19:06:40 -0700

    - Finished the KruskalMinimumSpanningTreeFinder implementation

--- mazes/src/graphs/minspantrees/KruskalMinimumSpanningTreeFinder.java
+++ mazes/src/graphs/minspantrees/KruskalMinimumSpanningTreeFinder.java
@@ -1,13 +1,15 @@
 package graphs.minspantrees;
 
 import disjointsets.DisjointSets;
-import disjointsets.QuickFindDisjointSets;
+import disjointsets.UnionBySizeCompressingDisjointSets;
 import graphs.BaseEdge;
 import graphs.KruskalGraph;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Computes minimum spanning trees using Kruskal's algorithm.
@@ -17,12 +19,12 @@ public class KruskalMinimumSpanningTreeFinder<G extends KruskalGraph<V, E>, V, E
     implements MinimumSpanningTreeFinder<G, V, E> {
 
     protected DisjointSets<V> createDisjointSets() {
-        return new QuickFindDisjointSets<>();
+        //return new QuickFindDisjointSets<>();
         /*
         Disable the line above and enable the one below after you've finished implementing
         your `UnionBySizeCompressingDisjointSets`.
          */
-        // return new UnionBySizeCompressingDisjointSets<>();
+        return new UnionBySizeCompressingDisjointSets<>();
 
         /*
         Otherwise, do not change this method.
@@ -31,6 +33,19 @@ public class KruskalMinimumSpanningTreeFinder<G extends KruskalGraph<V, E>, V, E
          */
     }
 
+    /*
+        kruskalMST(G graph)
+          Set(?) msts; Set finalMST;
+          initialize msts with each vertex as single-element MST
+          sort all edges by weight (smallest to largest)
+
+          for each edge (u,v) in ascending order:
+            uMST = msts.find(u)
+            vMST = msts.find(v)
+            if (uMST != vMST):
+              finalMST.add(edge (u, v))
+              msts.union(uMST, vMST)
+    */
     @Override
     public MinimumSpanningTree<V, E> findMinimumSpanningTree(G graph) {
         // Here's some code to get you started; feel free to change or rearrange it if you'd like.
@@ -39,9 +54,31 @@ public class KruskalMinimumSpanningTreeFinder<G extends KruskalGraph<V, E>, V, E
         List<E> edges = new ArrayList<>(graph.allEdges());
         edges.sort(Comparator.comparingDouble(E::weight));
 
-        DisjointSets<V> disjointSets = createDisjointSets();
+        List<V> vertices = new ArrayList<>(graph.allVertices());
 
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        DisjointSets<V> disjointSets = createDisjointSets();
+        Set<E> mst = new HashSet<>();
+
+        //call makeSet on each vertex to create subsets of disjoint set
+        for (V vertex : vertices) {
+            disjointSets.makeSet(vertex);
+        }
+        for (E edge : edges) {
+            V uMST = edge.from();
+            V vMST = edge.to();
+
+            if (disjointSets.findSet(uMST) != disjointSets.findSet(vMST)) {
+                mst.add(edge);
+                disjointSets.union(uMST, vMST);
+            }
+        }
+        //Check if the MST is completed: edge's amount in MST should be equal to vertices - 1
+        if (mst.size() == vertices.size() - 1 || edges.size() == 0) {
+            return new MinimumSpanningTree.Success<>(mst);
+        } else {
+            return new MinimumSpanningTree.Failure<>();
+        }
+
+
     }
 }
