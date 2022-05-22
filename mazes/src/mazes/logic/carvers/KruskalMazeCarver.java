c350b7e834159413fab4816f8faf94b671eec703
Jeremy Cao <you@example.com>
2022-05-21 19:25:38 -0700

    - Finished the KruskalMazeCarver implementation

--- mazes/src/mazes/logic/carvers/KruskalMazeCarver.java
+++ mazes/src/mazes/logic/carvers/KruskalMazeCarver.java
@@ -6,6 +6,7 @@ import mazes.entities.Room;
 import mazes.entities.Wall;
 import mazes.logic.MazeGraph;
 
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
@@ -29,11 +30,34 @@ public class KruskalMazeCarver extends MazeCarver {
         this.rand = new Random(seed);
     }
 
+    /*
+        - By randomizing the wall weights, we remove random walls which satisfy criterion 1.
+        - An MST, by definition, will include a path from every vertex (every room) to every other one,
+        satisfying criterion 2.
+        - And finally, because the MST will not have cycles,
+        we avoid removing unnecessary edges and end up with a maze where there really is only one solution,
+        satisfying criterion 3.
+     */
     @Override
     protected Set<Wall> chooseWallsToRemove(Set<Wall> walls) {
-        // TODO: replace this with your code
+
+        Set<EdgeWithData<Room, Wall>> edges = new HashSet<>();
+        Set<Wall> updatedWalls = new HashSet<>();
+
+        for (Wall wall : walls) {
+            double weight = rand.nextDouble();
+            EdgeWithData<Room, Wall> newEdge = new EdgeWithData<>(wall.getRoom1(), wall.getRoom2(), weight, wall);
+            edges.add(newEdge);
+        }
+        HashSet<EdgeWithData<Room, Wall>> newbie = (HashSet<EdgeWithData<Room, Wall>>)
+            this.minimumSpanningTreeFinder.findMinimumSpanningTree(new MazeGraph(edges)).edges();
         // Hint: you'll probably need to include something like the following:
-        // this.minimumSpanningTreeFinder.findMinimumSpanningTree(new MazeGraph(edges));
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+        for (EdgeWithData<Room, Wall> wall : newbie) {
+            updatedWalls.add(wall.data());
+        }
+
+        return updatedWalls;
+
     }
 }
